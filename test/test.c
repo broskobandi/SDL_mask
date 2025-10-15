cda3f6b54c37274efb78c0f663bdd07c9868cc69
broskobandi <broskobandi@protonmail.com>
2025-10-15 17:00:54 +0100

    Add ctest library in test file

--- test/test.c
+++ test/test.c
@@ -1,21 +1,21 @@
 #include "SDL_mask_priv.h"
-#include <assert.h>
+#include <ctest.h>
 
 int main(void) {
-	assert(!SDL_Init(SDL_INIT_EVERYTHING));
-	assert(IMG_Init(IMG_INIT_PNG));
+	CTEST(!SDL_Init(SDL_INIT_EVERYTHING));
+	CTEST(IMG_Init(IMG_INIT_PNG));
 
 	SDL_Surface *img = IMG_Load("../img_test.png");
-	assert(img);
+	CTEST(img);
 	SDL_Surface *mask = IMG_Load("../mask_test.png");
-	assert(mask);
+	CTEST(mask);
 	SDL_Surface *tmp = SDL_ConvertSurfaceFormat(img, SDL_PIXELFORMAT_RGBA8888, 0);
-	assert(tmp);
+	CTEST(tmp);
 	img = tmp;
 	tmp = SDL_ConvertSurfaceFormat(mask, SDL_PIXELFORMAT_RGBA8888, 0);
-	assert(tmp);
+	CTEST(tmp);
 	mask = tmp;
-	assert(mask->format == img->format);
+	CTEST(mask->format == img->format);
 
 	SDL_Color mask_col = {0, 0, 0, 0};
 	SDL_Color target_col = {0, 0, 0, 0};
@@ -40,16 +40,18 @@ int main(void) {
 				col.b == mask_col.b && col.a == mask_col.a)
 			{
 				SDL_GetRGBA(imgp, img->format, &col.r, &col.g, &col.b, &col.a);
-				assert(col.r == target_col.r);
-				assert(col.g == target_col.g);
-				assert(col.b == target_col.b);
-				assert(col.a == target_col.a);
+				CTEST(col.r == target_col.r);
+				CTEST(col.g == target_col.g);
+				CTEST(col.b == target_col.b);
+				CTEST(col.a == target_col.a);
 			}
 		}
 	}
 
 	IMG_Quit();
 	SDL_Quit();
+
+	ctest_print_results();
 
 	return 0;
 }
